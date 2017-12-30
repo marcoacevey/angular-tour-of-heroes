49c91cbf7623bbed42202896ece3ab9b4b654d8d
Marco Aurélio Cevey <marco.cevey@outlook.com>
2017-12-30 01:03:13 -0200

    Fim parte 6 - Services

---
+++ src/app/hero.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+
+import { Observable } from 'rxjs/Observable';
+import { of } from 'rxjs/observable/of';
+
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+import { MessageService } from './message.service';
+
+@Injectable()
+export class HeroService {
+
+  constructor(private messageService: MessageService) { }
+
+  /**
+   * @returns Uma coleção de Heróis;
+   */
+  getHeroes(): Observable<Hero[]> {
+    // todo: send the message _after_ fetching the heroes
+    this.messageService.add('HeroService: fetched heroes');
+    return of(HEROES);
+  }
+
+}
